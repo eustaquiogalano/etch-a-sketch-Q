96134460c2c83f895753e06a8c987ff17a665922
eustaquio <eustaquio.galano@gmail.com>
2024-11-20 12:19:30 +0800

    Add hover effect that changes the background color

--- script.js
+++ script.js
@@ -3,19 +3,34 @@ const sixteenBySixteenContainer = document.querySelector('#sixteen-by-sixteen-co
 /////////////////////////////////////////////////////////////////////////////////
 
 function createDivs() {
-
     let generateDivs = '';
 
     for (let i = 0; i < 256; i++) {
-        generateDivs += `<div id='divs'></div>`;
+        // create 256 pieces of div made of string
+        generateDivs += `<div class='divs div${i}'></div>`;
     }
 
+    // manipulate the container to insert all the div created
     sixteenBySixteenContainer.innerHTML = generateDivs;
-
     generateDivs = '';
+}
 
+function addListenersToDivs(listOfDivs) {  
+    // give each div eventlistener 
+    listOfDivs.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            // when mouse enters the div change background color
+            element.style.backgroundColor = 'black'
+        });
+    });
 }
 
 /////////////////////////////////////////////////////////////////////////////////
 
 createDivs();
+
+const rawDivs = document.querySelectorAll('.divs');
+const divs = Array.from(rawDivs);
+
+addListenersToDivs(divs);
+
